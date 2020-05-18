ddaec840acaff891cf21f14fb2a6631c94c79e5e
Victoria <vnkcampbell@gmail.com>
2020-05-17 23:41:34 -0700

    Add combs and bees routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'worker_bees/index'
+  root to: 'static_pages#root'
 
+  get 'worker_bees/index'
   root to: 'worker_bees#index'
-  resources :worker_bees, only: [:index, :show, :update]
+
+  namespace :api, defaults: {format: :json} do
+    resources :combs, only: [:show] do
+      resources :worker_bees, only: [:index, :show, :update]
+    end
+  end  
+  
 end
